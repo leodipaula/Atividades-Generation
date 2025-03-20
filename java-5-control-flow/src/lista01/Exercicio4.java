536080d72bd7321c833d987c325b7240b69efb00
leodipaula <leofernandes9@gnail.com>
2025-03-20 11:20:42 -0300

    corrigido o nome das variaveis do exercicio 4

--- java-5-control-flow/src/lista01/Exercicio4.java
+++ java-5-control-flow/src/lista01/Exercicio4.java
@@ -4,41 +4,41 @@ import java.util.Scanner;
 
 public class Exercicio4 {
     public static void main(String[] args) {
-        String caract1 = "", caract2 = "", caract3 = "";
+        String caractere1 = "", caractere2 = "", caractere3 = "";
         Scanner sc = new Scanner(System.in);
 
         try {
             System.out.println("Digite a primeira característica, sem acentos: ");
-            caract1 = sc.nextLine();
+            caractere1 = sc.nextLine();
 
             System.out.println("Digite a segunda característica, sem acentos: ");
-            caract2 = sc.nextLine();
+            caractere2 = sc.nextLine();
 
             System.out.println("Digite a terceira característica, sem acentos: ");
-            caract3 = sc.nextLine();
+            caractere3 = sc.nextLine();
 
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
             e.printStackTrace();
         }
 
-        verificarAnimalEscolhido(caract1, caract2, caract3);
+        verificarAnimalEscolhido(caractere1, caractere2, caractere3);
 
         sc.close();
     }
 
-    private static void verificarAnimalEscolhido(String caract1, String caract2, String caract3) {
-        if (caract1.equalsIgnoreCase("vertebrado"))
-            if (caract2.equalsIgnoreCase("ave"))
-                System.out.println(caract3.equalsIgnoreCase("carnivoro") ? "\nÁguia" : "\nPomba");
+    private static void verificarAnimalEscolhido(String caractere1, String caractere2, String caractere3) {
+        if (caractere1.equalsIgnoreCase("vertebrado"))
+            if (caractere2.equalsIgnoreCase("ave"))
+                System.out.println(caractere3.equalsIgnoreCase("carnivoro") ? "\nÁguia" : "\nPomba");
             else
-                System.out.println(caract3.equalsIgnoreCase("onivoro") ? "\nHomem" : "\nVaca");
+                System.out.println(caractere3.equalsIgnoreCase("onivoro") ? "\nHomem" : "\nVaca");
 
-        else if (caract1.equalsIgnoreCase("invertebrado"))
-            if (caract2.equalsIgnoreCase("inseto"))
-                System.out.println(caract3.equalsIgnoreCase("hematofago") ? "\nPulga" : "\nLagarta");
+        else if (caractere1.equalsIgnoreCase("invertebrado"))
+            if (caractere2.equalsIgnoreCase("inseto"))
+                System.out.println(caractere3.equalsIgnoreCase("hematofago") ? "\nPulga" : "\nLagarta");
             else
-                System.out.println(caract3.equalsIgnoreCase("hematofago") ? "\nSanguessuga" : "\nMinhoca");
+                System.out.println(caractere3.equalsIgnoreCase("hematofago") ? "\nSanguessuga" : "\nMinhoca");
         else
             System.out.println("Caracteres inválidos.");
     }
